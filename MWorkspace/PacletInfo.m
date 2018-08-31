182e929fa6b7165f9d6985f83e258c81a65e5bf2
miRoox <miroox@outlook.com>
2018-08-31 16:59:25 +0800

    Ver 0.2.0

---
+++ MWorkspace/PacletInfo.m
@@ -0,0 +1,24 @@
+(* ::Package:: *)
+
+(* Paclet Info File *)
+
+Paclet[
+	Name->"MWorkspace",
+	Version->"0.2.0",
+	MathematicaVersion->"11.3+",
+	Description->"A workspace like MATLAB.",
+	Creator->"miRoox <miRoox@outlook.com>",
+	Extensions->{
+		{
+			"FrontEnd",
+			"Prepend"->True
+		},
+		{
+			"Resource",
+			"Root"->"Resources",
+			"Resources"->{
+				"PaletteGenerators"
+			}
+		}
+	}
+]
